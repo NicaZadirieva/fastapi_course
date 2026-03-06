d17570f127c45185b1ebe010a5034230171ed20e
NicaZadirieva <no.demchenko@yandex.ru>
2026-03-06 10:12:29 +0300

    demo: пример работы с ошибками и статус кодами

--- no_ht/app/main.py
+++ no_ht/app/main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
+import random
+from fastapi import FastAPI, HTTPException, Response
 
 app = FastAPI()
 
 
 @app.get("/")
-def root():
-    return {"Score": 10}
+def root(response: Response):
+    num = random.random()
+    if num > 0.5:
+        raise HTTPException(401, "Не авторизован")
+    # return {"Score": 10}
+    else:
+        response.status_code = 201
+    return {"Score": num}
