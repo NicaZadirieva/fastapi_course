bf9aa8284fb91865da90dc746729d066689df24b
NicaZadirieva <no.demchenko@yandex.ru>
2026-03-06 10:15:03 +0300

    refactor: вынесла ошибку в отдельный класс

--- no_ht/app/main.py
+++ no_ht/app/main.py
@@ -4,11 +4,16 @@ from fastapi import FastAPI, HTTPException, Response
 app = FastAPI()
 
 
+class UnauthHTTPException(HTTPException):
+    def __init__(self) -> None:
+        super().__init__(401, "Не авторизован")
+
+
 @app.get("/")
 def root(response: Response):
     num = random.random()
     if num > 0.5:
-        raise HTTPException(401, "Не авторизован")
+        raise UnauthHTTPException()
     # return {"Score": 10}
     else:
         response.status_code = 201
