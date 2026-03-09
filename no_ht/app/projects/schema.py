24bbb4611e0ebd36b470d22a98908cb0b94b5375
NicaZadirieva <no.demchenko@yandex.ru>
2026-03-09 15:00:15 +0300

    demo: валидация поля key

--- no_ht/app/projects/schema.py
+++ no_ht/app/projects/schema.py
@@ -1,7 +1,15 @@
-from pydantic import BaseModel
+from fastapi import HTTPException
+from pydantic import BaseModel, field_validator
 
 
 class ProjectCreateRequest(BaseModel):
     key: str
     name: str
     description: str | None = None
+
+    @field_validator("key")
+    @classmethod
+    def key_not_empty(cls, value):
+        if not value.strip():
+            raise HTTPException(400, "key must be valid string")
+        return value
