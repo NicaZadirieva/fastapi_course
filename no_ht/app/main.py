96262ce8d847eed101f4152ff89e3be66ca07894
NicaZadirieva <no.demchenko@yandex.ru>
2026-03-11 13:23:15 +0300

    feat: tasks

--- no_ht/app/main.py
+++ no_ht/app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 from projects import routes as project_router
+from tasks import routes as tasks_router
 
 app = FastAPI(
     title="KanbanBoard API",
     description="API для работы приложения аналога JIRA",
     version="0.0.1",
-    openapi_tags=[{"name": "Projects", "description": "Управление проектами"}],
+    openapi_tags=[
+        {"name": "Projects", "description": "Управление проектами"},
+        {"name": "Tasks", "description": "Управление задачами"},
+    ],
 )
 app.include_router(project_router.router)
+app.include_router(tasks_router.router)
---
+++ no_ht/app/tasks/routes.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, Depends
+
+from .schema import TaskPath, TaskResponse
+
+
+router = APIRouter(prefix="/v1/tasks", tags=["Tasks"])
+
+
+@router.get("/{task_id}", response_model=TaskResponse, status_code=200)
+async def get_task(
+    path: TaskPath = Depends(),
+):
+    return TaskResponse(id=path.task_id)
