42f67e556b0bef545565ca7ba0a3d4a14cd0d7e5
NicaZadirieva <no.demchenko@yandex.ru>
2026-03-18 09:12:45 +0300

    demo: создание конфигурации через pydantic

---
+++ no_ht/app/core/settings.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class AppSettings(BaseModel):
+    app_name: str = "Board API"
+    debug: bool = False
+
+
+class DatabaseSettings(BaseModel):
+    url: str
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+    )
+    app_name: str = "Board API"
+    debug: bool = False
+
+    database_url: str = Field(validation_alias="DATABASE_URL")
+
+    @property
+    def db(self) -> DatabaseSettings:
+        return DatabaseSettings(url=self.database_url)
+
+    @property
+    def app(self) -> AppSettings:
+        return AppSettings(debug=self.debug, app_name=self.app_name)
