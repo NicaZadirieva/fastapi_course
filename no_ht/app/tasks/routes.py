7345edcfe022ba4282d8b5ab0fa544cbcb0ebd6b
NicaZadirieva <no.demchenko@yandex.ru>
2026-03-18 12:30:27 +0300

    demo: неправильное использование логов

--- no_ht/app/tasks/routes.py
+++ no_ht/app/tasks/routes.py
@@ -1,3 +1,4 @@
+import logging
 from fastapi import APIRouter, Depends
 
 from app.core.settings import SettingsDeps
@@ -8,6 +9,7 @@ from .schema import TaskPath, TaskResponse
 
 
 router = APIRouter(prefix="/v1/tasks", tags=["Tasks"])
+logger = logging.getLogger(__name__)
 
 
 @router.get("/{task_id}", response_model=TaskResponse, status_code=200)
@@ -16,4 +18,5 @@ async def get_task(
     settings: SettingsDeps,
     path: TaskPath = Depends(),
 ):
+    logger.warning(f"ID: {path.task_id}")
     return TaskResponse(id=path.task_id)
